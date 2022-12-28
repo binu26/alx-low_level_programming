17d193d5c4e779aad117f3442dc28f2d1740e9c7
binu26 <biniyam26melaku@gmail.com>
2022-12-28 11:58:43 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,14 +3,13 @@
 
 /**
  * main- prints "Programming is like building a multilingual
- *  puzzle, followed by a new line."
- *  return 0.
+ *  puzzle, followed by a new line.
+ *  Return  Always 0.
  */
 
 int main(void)
 
 {
-	    puts("\"Programming is like building a multilingual puzzle");
-
-	    return(0);
+	   puts("\"Programming is like building a multilingual puzzle");
+	    return (0);
 }
